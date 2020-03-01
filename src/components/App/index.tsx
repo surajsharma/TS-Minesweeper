9674008abe4e578bd6bd5563871e5667d6826074
suraj <surajsharma@pm.me>
2020-03-01 14:08:23 +0530

    first video done

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.scss";
 import NumberDisplay from "../NumberDisplay";
+import Button from "../Button";
+import { generateCells } from "../../utils/";
+
 const App: React.FC = () => {
+    const [cells, setCells] = useState(generateCells());
+
+    const renderCells = (): React.ReactNode => {
+        return cells.map((row, rowIndex) =>
+            row.map((cell, colIndex) => (
+                <Button key={`${rowIndex}-${colIndex}`} />
+            ))
+        );
+    };
+
     return (
         <div className="App">
             <div className="Header">
@@ -14,7 +27,7 @@ const App: React.FC = () => {
                 </div>
                 <NumberDisplay value={23} />
             </div>
-            <div className="Body">Body</div>
+            <div className="Body">{renderCells()}</div>
         </div>
     );
 };
