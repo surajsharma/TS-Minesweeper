ac3439cc702ee5bcddad4d85ef7be3b9270d5e0c
suraj <surajsharma@pm.me>
2020-03-01 10:12:36 +0530

    header ready

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 
 import "./App.scss";
-
+import NumberDisplay from "../NumberDisplay";
 const App: React.FC = () => {
-    return <div className="App">Hello From App</div>;
+    return (
+        <div className="App">
+            <div className="Header">
+                <NumberDisplay value={0} />
+                <div className="Face">
+                    <span role="img" aria-label="face">
+                        👽
+                    </span>
+                </div>
+                <NumberDisplay value={23} />
+            </div>
+            <div className="Body">Body</div>
+        </div>
+    );
 };
 
 export default App;
